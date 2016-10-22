b198a9124f68cbbcfe4f7707ade6607c7c3f5ed9
Ubuntu <ubuntu@ip-172-31-54-71.ec2.internal>
2016-10-22 21:07:07 +0000

    added check for negative buy. added basic currency validity check. TODO NEXT: fill in check currency

--- orders.js
+++ orders.js
@@ -19,22 +19,22 @@ var hello = "hello";
 //not the right way to do this. you need to type buy(10,usd) to do it right 
 //which is not on the output example
 
-var buy = function(amount, currency){
-
-	//no currency (it is an optional parameter)
-
-	if (currency == null){
-		//no currency, continue on for buying amount of bitcoin.
-        	console.log("order to buy " + amount + " BTC queued.");
-		//todo save into a variable or something to do order function.
-	}
-
-	else{
-		//buy amount at the currency rate
-		var rate = 1000000;
-		console.log("Order to buy " + amount + currency + " of BTC queued @ " + rate );
-	}	
-}
+//var buy = function(amount, currency){
+//
+//	//no currency (it is an optional parameter)
+//
+//	if (currency == null){
+//		//no currency, continue on for buying amount of bitcoin.
+//      	console.log("order to buy " + amount + " BTC queued.");
+//		//todo save into a variable or something to do order function.
+//	}
+//
+//	else{
+//		//buy amount at the currency rate
+//		var rate = 1000000;
+//		console.log("Order to buy " + amount + currency + " of BTC queued @ " + rate );
+//	}	
+//}
 
 // ~~~~~~~~~~~~Start REPL mode~~~~~~~~~~~~~~~~~~~~
 
@@ -50,10 +50,49 @@ function evaluate(cmd, context, fileName, callback){
 	var exe = cmds[0].trim();
 
 	//these will be the commands that are supported
-	if(exe == "buy"){console.log("You executed the buy command.");}
+	if(exe == "buy"){
+	return buy(cmds);}
+//	console.log("You executed the buy command.");}
 	if(exe == "sell"){console.log("You executed the sell command");}
 	if(exe == "orders"){console.log("You executed the orders command");}
+	callback(null, result);
+	return;	
+}
 
+// buy function, buy the remaining arguments (amount, currency)
+function buy(rem){
+	var x = rem[0].trim();
+
+	//check for valid command (buy 10, buy 10 usd). aka MUST have a 2nd arg
+	//TODO check for valid (not buy asdf)
+	if(rem[1]==null || rem[1]<0){console.log("A buy amount was not input or the input was negative.");}
+	
+	else{
+		var amount = rem[1].trim();
+	
+		//case 1 there is no currency
+		if(rem[2] == null){
+			console.log("Order to buy " + amount + " BTC queued.");
+		} 
+
+		//case 2 there is a currency
+		if(rem[2]!=null){
+			var currency = rem[2].trim();
+	
+			if(isCurrencyValid(currency)){ 
+				//add to order TODO
+				console.log("Order to buy " + amount + " " + currency + " worth of BTC queued.");
+			}
+			else{ console.log("please enter a valid currrency type"); }
+		}
+	}
+	return;
+}
+
+//TEST for usd first, add in others later from the coinbase api
+function isCurrencyValid(currency){
+	if(currency == "usd"){return true;}
+	else return false;
 }
 
 

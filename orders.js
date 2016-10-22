ab4383eef5f0f55a508ea1d95392c16f3ff46d41
Ubuntu <ubuntu@ip-172-31-54-71.ec2.internal>
2016-10-22 19:20:25 +0000

    testing repl. why does it auto do the request? find out how to go back to the prompt

--- orders.js
+++ orders.js
@@ -1,10 +1,14 @@
+repl = require('repl');
 var request = require('request');
 
-request('https://coinbase.com/api/v1/currencies/exchange_rates',function(error,response,body){
+var test = request('https://coinbase.com/api/v1/currencies/exchange_rates',function(error,response,body){
 	if(!error && response.statusCode == 200){
 		console.log(body)
 	}
 });
 
+var a = repl.start('coinbase>');
+//a.context.test = test;
+//a.context.buy = buy;
 
-request.get({});
+//request.get({});

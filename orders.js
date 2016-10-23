fd95cf37ee720861fb4195983a682ffb115b906e
Krystle Weinrich <krystleweinrich@yahoo.com>
2016-10-23 02:03:14 +0000

    validity works :)

--- orders.js
+++ orders.js
@@ -13,136 +13,161 @@ var orderList = [];
 
 // ~~~~~~~~~~~~Start REPL mode~~~~~~~~~~~~~~~~~~~~
 
-var r = repl.start({ prompt: 'coinbase>', eval: evaluate });
+var r = repl.start({
+    prompt: 'coinbase>',
+    eval: evaluate
+});
 
 // ~~~~~~~~~~REPL functions~~~~~~~~~~~~~~~~~~~~~~
 
 // evaluate each thing put into the repl command prompt
-function evaluate(cmd, context, fileName, callback){
-        //split commands by spaces
-        var cmds = cmd.split(" ");
+function evaluate(cmd, context, fileName, callback) {
+    //split commands by spaces
+    var cmds = cmd.split(" ");
 
-        //execute current command
-        var exe = cmds[0].trim().toLowerCase();
+    //execute current command
+    var exe = cmds[0].trim().toLowerCase();
 
-        //these will be the commands that are supported
-        if(exe == "buy"){return buy(cmds);}
-        if(exe == "sell"){return sell(cmds);}
-        if(exe == "orders"){return orders();}
-        callback(null, result);
-        return;
+    //these will be the commands that are supported
+    if (exe == "buy") {
+        return buy(cmds);
+    }
+    if (exe == "sell") {
+        return sell(cmds);
+    }
+    if (exe == "orders") {
+        return orders();
+    }
+    callback(null, result);
+    return;
 
 }
 
 // buy function, buy the remaining arguments (amount, currency)
-function buy(rem){
-        var actionType = rem[0].trim();
+function buy(rem) {
+    var actionType = rem[0].trim();
 
-        //check for valid command (buy 10, buy 10 usd). aka MUST have a 2nd arg
-        //TODO check for valid (not buy asdf)
-        if(rem[1]==null || rem[1]<0){console.log("A buy amount was not input or the input was negative.");}
+    //check for valid command (buy 10, buy 10 usd). aka MUST have a 2nd arg
+    //TODO check for valid (not buy asdf)
+    if (rem[1] == null || rem[1] < 0) {
+        console.log("A buy amount was not input or the input was negative.");
+    }
 
-        else{
-            var amount = rem[1].trim();
+    else {
+        var amount = rem[1].trim();
 
-            //case 1 there is no currency
-            if(rem[2] == null){
-                addToOrders(actionType,amount,null);
-                console.log("Order to buy " + amount + " BTC queued.");
-            }
-           //case 2 there is a currency
-            else{
-                var currency = rem[2].trim();
-                    //this is NOT valid currency
-                        if(!isCurrencyValid(currency)){
-                            console.log("please enter a valid currrency type"); 
-                        }
-                        //this IS valid currency
-                        else{ 
-                           // var exrate = getBuyExchangeRate(amount,currency);
-                           addToOrders(actionType, amount, currency);
-                           getExchangeRate(actionType,amount,currency);
-                        }
-                }
+        //case 1 there is no currency
+        if (rem[2] == null) {
+            addToOrders(actionType, amount, null);
+            console.log("Order to buy " + amount + " BTC queued.");
         }
-        return;
-}
-
-function isCurrencyValid(currency){
-        return true;
-        
-        //why doesnt this work??
-        var validity = false;
-        
-         superagent.get('https://api.coinbase.com/v1/currencies')
-          .set('Accept', 'application/json')
-          .end(function(error, res){
-              if (error){return;}
-              var holder = res.body;
-              holder.forEach(function (curr) {
-                  // curr = [ 'Gibraltar Pound (GIP)', 'GIP' ]
-                  if (curr[1] === currency) {validity = true;}   
-              });
-        });
-        return validity;
-}
-
-function getExchangeRate(actionType,amount,currency){
-      var keyword_btccur= "btc_to_" + currency;
-      var keyword_curbtc= currency + "_to_btc";
-        superagent.get("https://api.coinbase.com/v1/currencies/exchange_rates")
-          .set('Accept', 'application/json')
-          .end(function(error, res){
-                var holder = res.body;
-                if(currency != null){
-    
-                  var div_btc_curr = holder[keyword_btccur];
-                  var div_curr_btc = holder[keyword_curbtc];
-                  console.log("Order to " + actionType + " " + amount + " " + currency + " " + "worth of BTC queued @ " + 
-                    div_btc_curr + " " + "BTC/" + currency.toUpperCase() + " ("+ div_curr_btc + " BTC)");
+        //case 2 there is a currency
+        else {
+            var currency = rem[2].trim();
+            //this is NOT valid currency
+            isCurrencyValid(currency, function(error, response) {
+                if (error) {
+                    console.log(error);
                 }
-                });
-                
+                else {
+                    addToOrders(actionType, amount, currency);
+                    getExchangeRate(actionType, amount, currency);
+
+                }
+
+            });
+        }
+    }
+    return;
 }
 
-function sell(rem){
+function isCurrencyValid(currency, callback) {
+    // return true;
+
+    superagent.get('https://api.coinbase.com/v1/currencies')
+        .set('Accept', 'application/json')
+        .end(function(error, res) {
+            if (error) {
+                return;
+            }
+            var holder = res.body;
+            var valid = false;
+            holder.forEach(function(curr) {
+                // curr = [ 'Gibraltar Pound (GIP)', 'GIP' ]
+                if (curr[1] === currency.toUpperCase()) {
+                    valid = true;
+                }
+            });
+            if (valid === true) {
+                callback(null, true);
+            }
+            else {
+                callback("invalid currency");
+            }
+            //console.log(validity);
+        });
+}
+
+function getExchangeRate(actionType, amount, currency) {
+    var keyword_btccur = "btc_to_" + currency.toLowerCase();
+    var keyword_curbtc = currency.toLowerCase() + "_to_btc";
+    superagent.get("https://api.coinbase.com/v1/currencies/exchange_rates")
+        .set('Accept', 'application/json')
+        .end(function(error, res) {
+            var holder = res.body;
+            if (currency != null) {
+
+                var div_btc_curr = holder[keyword_btccur];
+                var div_curr_btc = holder[keyword_curbtc];
+                console.log("Order to " + actionType + " " + amount + " " + currency + " " + "worth of BTC queued @ " +
+                    div_btc_curr + " " + "BTC/" + currency.toUpperCase() + " (" + div_curr_btc + " BTC)");
+            }
+        });
+
+}
+
+function sell(rem) {
     //for exchange rate single function (not buy and sell seperated)
     var actionType = rem[0].trim();
 
-        //check for valid command (sell 10, buy 10 usd). aka MUST have a 2nd arg
-        //TODO check for valid (not buy asdf)
-        if(rem[1]==null || rem[1]<0){console.log("There was no sell amount or the amount was negative.");}
+    //check for valid command (sell 10, buy 10 usd). aka MUST have a 2nd arg
+    //TODO check for valid (not buy asdf)
+    if (rem[1] == null || rem[1] < 0) {
+        console.log("There was no sell amount or the amount was negative.");
+    }
 
-        else{
-                var amount = rem[1].trim();
+    else {
+        var amount = rem[1].trim();
 
-                //case 1 there is no currency
-                if(rem[2] == null){     
-                    addToOrders(actionType,amount,null);
-                    console.log("Order to sell " + amount + " BTC queued.");
-                }
-
-                //case 2 there is a currency
-                else{
-                        var currency = rem[2].trim();
-
-                                //this is valid currency
-                                if(isCurrencyValid(currency)){
-                                    addToOrders(actionType, amount, currency);
-                                   getExchangeRate(actionType,amount,currency);
-                                    
-                                }
-//                              var exrate = getCurrencyRate(currency);
-                                    
-                              else{ console.log("please enter a valid currrency type"); }
-                }
+        //case 1 there is no currency
+        if (rem[2] == null) {
+            addToOrders(actionType, amount, null);
+            console.log("Order to sell " + amount + " BTC queued.");
         }
-        return;
-    
+
+        //case 2 there is a currency
+        else {
+            var currency = rem[2].trim();
+            //this is NOT valid currency
+            isCurrencyValid(currency, function(error, response) {
+                if (error) {
+                    console.log(error);
+                }
+                else {
+                    addToOrders(actionType, amount, currency);
+                    getExchangeRate(actionType, amount, currency);
+
+                }
+
+            });
+        }
+    }
+    return;
+
 }
 
-function addToOrders(actionType,amount,currency){
-    if (currency==null){
+function addToOrders(actionType, amount, currency) {
+    if (currency == null) {
         var newOrder = {
             'timeDate': new Date(),
             'type': actionType,
@@ -152,7 +177,7 @@ function addToOrders(actionType,amount,currency){
         };
         orderList.push(newOrder);
     }
-    else{
+    else {
         var newOrder = {
             'timeDate': new Date(),
             'type': actionType,
@@ -164,11 +189,11 @@ function addToOrders(actionType,amount,currency){
     }
 }
 
-function orders(){
-    console.log("\n === CURRENT ORDERS ===" );
-    
-   orderList.forEach(function(o){
-       console.log(o.timeDate + " : " + o.type + " "
-                + o.amount + o.currency+" :  " + o.status);
-   });
+function orders() {
+    console.log("\n === CURRENT ORDERS ===");
+
+    orderList.forEach(function(o) {
+        console.log(o.timeDate + " : " + o.type + " " +
+            o.amount + o.currency + " :  " + o.status);
+    });
 }
